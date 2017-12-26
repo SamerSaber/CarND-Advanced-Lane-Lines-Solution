f7a86f545dd188935b471da845b4733e862770a1
SamerSaber <eng.samer.saber@gmail.com>
2017-12-26 03:15:36 +0200

    first stable video

--- advanced_lane_finder.py
+++ advanced_lane_finder.py
@@ -5,8 +5,11 @@ import preprocessing
 import line_extractor
 import matplotlib.pyplot as plt
 import numpy as np
-def process_undistorted_frame(frame, DebugImage = False):
+from moviepy.editor import VideoFileClip
+def process_frame(frame, DebugImage = False):
     
+    #undistort the frame
+    frame = camera_calibration.undistort(frame, cam_mtx, distortion)
     
     bin_image = preprocessing.process(frame, DebugImage)
     #prespective transformation
@@ -24,8 +27,27 @@ def process_undistorted_frame(frame, DebugImage = False):
     warped , M, Minv = prespective_transform.transform(bin_image, src_pts, dst_pts, DebugImage)
     
     #Extract the lane lines
-    line_extractor.extract(warped, DebugImage)
+    left_fitx , right_fitx , ploty = line_extractor.extract(warped, DebugImage)
     
+    #visualize the output
+    # Create an image to draw the lines on
+    warp_zero = np.zeros_like(warped).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+    
+    # Recast the x and y points into usable format for cv2.fillPoly()
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+    
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+    
+    # Warp the blank back to original image space using inverse perspective matrix (Minv)
+    newwarp = cv2.warpPerspective(color_warp, Minv, (frame.shape[1], frame.shape[0])) 
+    # Combine the result with the original image
+    result = cv2.addWeighted(frame, 1, newwarp, 0.3, 0)
+    plt.imshow(result)
+#     plt.show()
 #     if DebugImage:
 #         
 #         
@@ -42,14 +64,27 @@ def process_undistorted_frame(frame, DebugImage = False):
 #         plt.show()
     
     
-    return
+    return result
 
 
 
 if __name__ == '__main__':
-    #Calibrate the camera 
-    cam_mtx, distortion = camera_calibration.calibrate('./camera_cal')
     
-    #undistort the frame
-    frame = camera_calibration.undistort(cv2.imread('./test_images/test3.jpg'), cam_mtx, distortion)
-    process_undistorted_frame(frame, True)
+    static_image = True
+    cam_mtx, distortion = camera_calibration.calibrate('./camera_cal')
+    if static_image:
+        #Calibrate the camera 
+        frame = cv2.imread('./test_images/test1.jpg')
+        
+        process_frame(frame, True)
+        
+    else:
+        
+    
+        white_output = './test_videos_output/project_video.mp4'
+        
+        clip1 = VideoFileClip("./project_video.mp4")
+        
+        #clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4")
+        white_clip = clip1.fl_image(process_frame) #NOTE: this function expects color images!!
+        white_clip.write_videofile(white_output, audio=False)
--- line_extractor.py
+++ line_extractor.py
@@ -2,6 +2,23 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+def calcCurvature(left_fitx , right_fitx , ploty):
+    #########################Measure Curvature #######################
+    y_eval = np.max(ploty)
+    # Define conversions in x and y from pixels space to meters
+    ym_per_pix = 30/720 # meters per pixel in y dimension
+    xm_per_pix = 3.7/700 # meters per pixel in x dimension
+    
+    # Fit new polynomials to x,y in world space
+    left_fit_cr = np.polyfit(ploty*ym_per_pix, left_fitx*xm_per_pix, 2)
+    right_fit_cr = np.polyfit(ploty*ym_per_pix, right_fitx*xm_per_pix, 2)
+    # Calculate the new radii of curvature
+    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+    # Now our radius of curvature is in meters
+    print(left_curverad, 'm', right_curverad, 'm')
+    return
+
 def extract(binary_warped , Debug_Image = False):
     # Assuming you have created a warped binary image called "binary_warped"
     # Take a histogram of the bottom half of the image
@@ -15,7 +32,7 @@ def extract(binary_warped , Debug_Image = False):
     rightx_base = np.argmax(histogram[midpoint:]) + midpoint
     
     # Choose the number of sliding windows
-    nwindows = 15
+    nwindows = 25
     # Set height of windows
     window_height = np.int(binary_warped.shape[0]/nwindows)
     # Identify the x and y positions of all nonzero pixels in the image
@@ -82,6 +99,10 @@ def extract(binary_warped , Debug_Image = False):
     
     out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
     out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+    
+    calcCurvature(left_fitx , right_fitx , ploty)
+    
+    return left_fitx , right_fitx , ploty
     
     if Debug_Image:
         
